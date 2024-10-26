4dcaa5a1a666bfb9b2f7cb852890a921f5252c0e
54911 <edutormakh@gmail.com>
2024-10-26 05:23:29 -0300

    hola

--- ejlassers/Main.java
+++ ejlassers/Main.java
@@ -4,8 +4,6 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-Class
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,19 +16,31 @@ public class Main {
         // Number of beams sent by B
         int M = scanner.nextInt();
 
-        Set<> A = new HashSet();
+        Set<LinearFunction> A = new HashSet<>();
         for (int i=0; i < N ; i++) {
             String direction = scanner.next();
             int steps = scanner.nextInt();
+            letterChanger changer = letterChanger.valueOf(direction);  // Convierte la dirección a enum
+            int directionValue = changer.getValue();                   // Obtén el valor del enum
 
-            A.add();
+            // Crear la función lineal con la pendiente basada en steps y directionValue
+            LinearFunction linearFunction = new LinearFunction((double) steps / directionValue, 0);
+            A.add(linearFunction);
         }
+
+
+        Set<LinearFunction> B = new HashSet<>();
         for (int i=0; i < M ; i++) {
             String direction = scanner.next();
             int steps = scanner.nextInt();
+            letterChanger changer = letterChanger.valueOf(direction);  // Convierte la dirección a enum
+            int directionValue = changer.getValue();                   // Obtén el valor del enum
 
+            // Crear la función lineal con la pendiente basada en steps y directionValue
+            LinearFunction linearFunction = new LinearFunction((double) steps / directionValue, 0);
+            B.add(linearFunction);
         }
-        System.out.println("Direction: " + direction + ", Steps: " + steps);
+
 
 
         scanner.close();
---
+++ ejlassers/letterChanger.java
@@ -0,0 +1,19 @@
+package ejlassers;
+
+public enum letterChanger {
+    U(1),  // Por ejemplo, podrías decidir que U vale 1
+    R(2),  // R vale 2
+    L(3);  // L vale 3
+
+    private final int value;
+
+    // Constructor
+    letterChanger(int value) {
+        this.value = value;
+    }
+
+    // Método para obtener el valor numérico
+    public int getValue() {
+        return value;
+    }
+}
