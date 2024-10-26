dfeeae2f06e5d2ae0348d6180b523fefd5b106d1
DinosaurioBlue <franco21bonacina07@gmail.com>
2024-10-25 21:39:12 -0300

    push standar code for two fridges

---
+++ TwoFridges.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class TwoFridges {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
